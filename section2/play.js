35a8e2d0d64c65741cd825f9b822b290354bd053
ksp086 <dev.kashyap.patel.086@gmail.com>
2022-09-20 17:20:54 +0530

    Arrow Function

--- section2/play.js
+++ section2/play.js
@@ -4,7 +4,7 @@ const hasHobbies = true;
 
 age = 20;
 
-const summarizeUser = function (userName, userAge, userHobby) {
+const summarizeUser = (userName, userAge, userHobby) => {
     return (
         'Name is ' +
         userName +
@@ -15,3 +15,17 @@ const summarizeUser = function (userName, userAge, userHobby) {
     );
 }
 console.log(summarizeUser(name, age, hasHobbies));
+
+const add1 = (a, b) => {
+    return a + b;
+};
+console.log(add1(4, 4));
+
+const add2 = (a, b) => a + b;
+console.log(add2(3, 4));
+
+const addOne = a => a + 1;
+console.log(addOne(38));
+
+const addRandom = () => 1 + 2;
+console.log(addRandom());
