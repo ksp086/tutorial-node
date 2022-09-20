e2f4d5e0fa1eab9d0b77b3f2f0a88708397781a3
ksp086 <dev.kashyap.patel.086@gmail.com>
2022-09-20 17:25:34 +0530

    Arrow Function and Object Global scope of this

--- section2/play.js
+++ section2/play.js
@@ -1,31 +1,8 @@
-const name = 'Kashyap';
-let age = 21;
-const hasHobbies = true;
-
-age = 20;
-
-const summarizeUser = (userName, userAge, userHobby) => {
-    return (
-        'Name is ' +
-        userName +
-        ', age is ' +
-        userAge +
-        ' and the user has hobbies: ' +
-        userHobby
-    );
-}
-console.log(summarizeUser(name, age, hasHobbies));
-
-const add1 = (a, b) => {
-    return a + b;
+const person = {
+    name: 'Kashyap',
+    age: 21,
+    greet: () => {
+        console.log('Hi, i am ' + this.name);
+    }
 };
-console.log(add1(4, 4));
-
-const add2 = (a, b) => a + b;
-console.log(add2(3, 4));
-
-const addOne = a => a + 1;
-console.log(addOne(38));
-
-const addRandom = () => 1 + 2;
-console.log(addRandom());
+person.greet();
