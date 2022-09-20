0d4f82a4822e184457aa8a39a91fdd77eb1e0582
ksp086 <dev.kashyap.patel.086@gmail.com>
2022-09-20 17:39:37 +0530

    Arrays and Array Methods

--- section2/play.js
+++ section2/play.js
@@ -1,8 +1,15 @@
 const person = {
     name: 'Kashyap',
     age: 21,
-    greet() {
+    greet () {
         console.log('Hi, i am ' + this.name);
     }
 };
 person.greet();
+
+const hobbies = ['Sports', 'Cooking'];
+// for (let hobby of hobbies) {
+//     console.log(hobby);
+// }
+console.log(hobbies);
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
